eba3a5cae7f4d04e88ae11e2f5f36900225c7481
Gwen-JW <jiawennw@gmail.com>
2023-10-20 20:44:07 +0800

    initial init.el

---
+++ init.el
@@ -0,0 +1,43 @@
+;;; init.el --- Load the full configuration -*- lexical-binding: t -*-
+;;; Commentary:
+
+;; This file bootstraps the configuration, which is divided into
+;; a number of other files.
+
+;;; Code:
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+;; (require 'init-benchmarking) ;; Measure startup time
+
+(defconst *spell-check-support-enabled* t) ;; Disabled with nil if you prefer
+(defconst *is-a-mac* (eq system-type 'darwin))
+(defconst *is-a-linux* (eq system-type 'gnu/linux))
+
+;; Adjust garbage collection thresholds during startup, and thereafter
+
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+(setq confirm-kill-emacs #'yes-or-no-p)
+(electric-pair-mode t)
+(setq inhibit-startup-screen t)
+(global-display-line-numbers-mode 1)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(eval-when-compile
+  (require 'use-package))
+
+(require 'init-org)
+(require 'init-roam)
+(when *spell-check-support-enabled*
+  (require 'init-spelling))
+
+(provide 'init)
+
+;;; init.el ends here
