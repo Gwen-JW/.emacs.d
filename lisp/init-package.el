322b1977e43d4ecb1e922721901b671046614a44
Gwen-JW <jiawennw@gmail.com>
2023-10-21 16:05:58 +0800

    updated init-package.el

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -14,10 +14,7 @@
 (package-initialize)
 
 ;; Should set before loading `use-package'
-(setq use-package-always-ensure t
-      use-package-always-defer t
-      use-package-expand-minimally t
-      use-package-enable-imenu-support t)
+(setq use-package-always-ensure t)
 
 (when (not (package-installed-p 'paradox))
   (package-install 'paradox))
