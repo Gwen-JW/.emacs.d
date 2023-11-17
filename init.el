8cb18b6edf9b5a547de65cbc9f6b0851abdb24a4
Gwen-JW <jiawennw@gmail.com>
2023-11-17 12:50:53 +0800

    updated

--- init.el
+++ init.el
@@ -36,3 +36,16 @@
 (provide 'init)
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-agenda writeroom-mode paradox org-roam-ui org-modern org-download markdown-mode consult-org-roam)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
