07d6ed655eeeb31586d715d43795c6da63a08015
Gwen-JW <jiawennw@gmail.com>
2023-11-20 16:01:29 +0800

    add theme config

---
+++ lisp/init-theme.el
@@ -0,0 +1,12 @@
+;;; init-theme.el --- theme config -*- lexical-binding: t;-*-
+;;; Commentary:
+
+;;; Code:
+
+(use-package nord-theme
+  :config
+  (load-theme 'nord :t)
+  )
+
+(provide 'init-theme)
+;;; init-theme.el ends here
