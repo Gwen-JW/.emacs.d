e751162ab0196015afe62852ce240d37bbdeb51a
Gwen-JW <jiawennw@gmail.com>
2023-11-20 16:02:36 +0800

    add utils config

---
+++ lisp/init-utils.el
@@ -0,0 +1,11 @@
+;;; init-utils.el utils  -*- lexical-binding: t; -*-
+
+;;; Code
+
+(use-package which-key
+  :config
+  (which-key-mode t)
+  (which-key-setup-side-window-bottom))
+
+(provide 'init-utils)
+;;; init-utils.el ends here
